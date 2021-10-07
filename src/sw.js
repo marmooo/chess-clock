e78986880cea7871b27132e11e6b69835298c517
marmooo <marmooo@users.noreply.github.com>
2021-10-08 00:35:04 +0900

    bump bootstrap from 5.1.1 to 5.1.2

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,11 @@
-var CACHE_NAME = "2021-10-02 14:56";
+var CACHE_NAME = '2021-10-08 00:19';
 var urlsToCache = [
   "/chess-clock/",
   "/chess-clock/mp3/warning1.mp3",
   "/chess-clock/mp3/menu1.mp3",
   "/chess-clock/mp3/decision3.mp3",
   "/chess-clock/index.js",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/nosleep.js@0.12.0/dist/NoSleep.min.js",
 ];
 
